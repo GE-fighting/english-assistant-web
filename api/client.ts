008e9d35e4a11c4771c54633c5d07b804a8ad62b
zhangj <1501790343@qq.com>
2024-12-14 01:04:01 +0800

    feat: add dockerfile

--- api/client.ts
+++ api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://115.120.238.73:7789/api',
   headers: {
     'Content-Type': 'application/json',
   },
