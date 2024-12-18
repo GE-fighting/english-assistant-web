3522fb9d128468517981458ba201aeb7c85f31de
zhangj <1501790343@qq.com>
2024-12-18 14:29:40 +0800

    feat: introduce pm2

--- api/client.ts
+++ api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://115.120.238.73:7789/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
